efeca901ea0a6948776f015d2bdb7f9e5665062c
HP <U292148@u292148n1.lsy.bud.dlh.de>
2011-05-27 08:52:10 +0200

    transformer issues.

--- src/main/java/net/hp/st/BBFundEntry.java
+++ src/main/java/net/hp/st/BBFundEntry.java
@@ -1,23 +1,39 @@
 package net.hp.st;
 
+import java.util.Date;
+
 public class BBFundEntry {
 
   public String name;
   public double value;
   public String currency;
+  private Date publishedDate;
 
   public BBFundEntry(String name, double value, String currency) {
+    this(name, value, currency, null);
+  }
+
+  public BBFundEntry(String name, double value, String currency, Date date) {
     super();
     this.name = name;
     this.value = value;
     this.currency = currency;
+    this.publishedDate = date;
+  }
+
+  public Date getPublishedDate() {
+    return publishedDate;
+  }
+
+  public void setPublishedDate(Date publishedDate) {
+    this.publishedDate = publishedDate;
   }
 
   @Override
   public String toString() {
     StringBuilder builder = new StringBuilder();
     builder.append("BBFundEntry [name=").append(name).append(", value=").append(value).append(", currency=")
-        .append(currency).append("]");
+        .append(currency).append(", publishedDate=").append(publishedDate).append("]");
     return builder.toString();
   }
 
--- src/main/java/net/hp/st/SyndItemTransFormer.java
+++ src/main/java/net/hp/st/SyndItemTransFormer.java
@@ -19,6 +19,9 @@ public class SyndItemTransFormer {
     // [1.3322HUF] --- Kötvény [4.3143HUF] --- Nemz.Részv. [0.7018HUF] ---
     // Növekedési [2.6565HUF] --- Pénzpiaci [1.179952HUF] ---
     List<BBFundEntry> arrayList = parseEntry(syndEntry.getDescription().getValue());
+    for (BBFundEntry bbFundEntry : arrayList) {
+      bbFundEntry.setPublishedDate(syndEntry.getPublishedDate());
+    }
     return arrayList;
   }
 
@@ -30,37 +33,44 @@ public class SyndItemTransFormer {
   private List<BBFundEntry> parseEntry(String priceLine) {
     List<BBFundEntry> ret = new ArrayList<BBFundEntry>();
 
-    String[] priceItems = priceLine.split(" --- ");
+    String[] priceItems = priceLine.split("\\s+---\\s+");
 
     for (String str : priceItems) {
+      String[] items = str.split("\\s*\\[");
 
-      String[] items = str.split("\\s\\[");
       String name = items[0];
-
-      Matcher matcher1 = COMPILE1.matcher(items[1]);
-
-      Double value;
-      if (matcher1.find()) {
-        value = Double.valueOf(matcher1.group());
-      } else {
-        value = 0.0;
-      }
-
-      Pattern pattern2 = COMPILE2;
-      Matcher matcher2 = pattern2.matcher(items[1]);
-
-      String currency;
-      if (matcher2.find()) {
-        currency = matcher2.group();
-      } else {
-        currency = "XXX";
-      }
+      Double value = getValue(items[1]);
+      String currency = getCurrency(items[1]);
 
       ret.add(new BBFundEntry(name, value, currency));
-
     }
 
     return ret;
+  }
+
+  private String getCurrency(String item) {
+    Pattern pattern2 = COMPILE2;
+    Matcher matcher2 = pattern2.matcher(item);
+
+    String currency;
+    if (matcher2.find()) {
+      currency = matcher2.group();
+    } else {
+      currency = "XXX";
+    }
+    return currency;
+  }
+
+  private Double getValue(String item) {
+    Matcher matcher1 = COMPILE1.matcher(item);
+
+    Double value;
+    if (matcher1.find()) {
+      value = Double.valueOf(matcher1.group());
+    } else {
+      value = 0.0;
+    }
+    return value;
   }
 
 }
